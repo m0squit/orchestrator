d3944eb07e6e524f1a574757042f53f4f37b8eab
m0squit <platyna@bk.ru>
2021-08-03 19:49:57 +0500

    docs

--- config.py
+++ config.py
@@ -4,8 +4,25 @@ from typing import List
 
 class Config(object):
     """Настройки расчета для конкретного месторождения.
-    """
 
+    Parameters
+    ----------
+    field_name: str
+        Название месторождения на русском.
+
+    shops: List[str]
+        Список названий цехов на русском.
+
+    date_start: datetime.date
+        Дата начала адаптации моделей (начало с 00:00 этой даты).
+
+    date_test: datetime.date
+        Дата начала прогноза моделей (начало с 00:00 этой даты).
+
+    date_end: datetime.date
+        Дата конца прогноза моделей (конец по 23:59 этой даты).
+
+    """
     def __init__(
             self,
             field_name: str,
@@ -14,15 +31,6 @@ class Config(object):
             date_test: datetime.date,
             date_end: datetime.date,
     ):
-        """Инициализация класса Config.
-
-        Args:
-            field_name: Название месторождения на русском.
-            shops: Список названий цехов на русском.
-            date_start: Дата начала адаптации моделей (начало с 00:00 этой даты).
-            date_test: Дата начала прогноза моделей (начало с 00:00 этой даты).
-            date_end: Дата конца прогноза моделей (конец по 23:59 этой даты).
-        """
         self.field_name = field_name
         self.shops = shops
         self.date_start = date_start
