59525754f65fcbd8eb02697d720c6c67c635d66e
Konstantin Krechetov <Constantine.j.corp@gmail.com>
2021-08-12 16:50:55 +0500

    feature - add deviations to plots

--- plots.py
+++ plots.py
@@ -1,11 +1,20 @@
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
 
+_MODEL_NAMES = {
+    'ftor': 'Модель пьезо',
+    'wolfram': 'Модель ML',
+    'CRM': 'Модель CRM',
+    'true': 'Фактический дебит',
+}
+
+
 def compute_deviations(y_true: pd.Series, y_pred: pd.Series) -> pd.Series:
-    devs = abs(y_true - y_pred) / y_true
+    devs = np.abs(y_true - y_pred) / np.maximum(y_true, y_pred) * 100
     return devs
 
 
@@ -15,7 +24,7 @@ def create_well_plot(df_draw_liq,
                      date_test,
                      wellname=''):
     fig = make_subplots(
-        rows=3,
+        rows=4,
         cols=1,
         shared_xaxes=True,
         vertical_spacing=0.07,
@@ -23,6 +32,7 @@ def create_well_plot(df_draw_liq,
         subplot_titles=[
             'Дебит жидкости, м3',
             'Дебит нефти, м3',
+            'Относительная ошибка по нефти, %',
             'Забойное давление, атм',
         ]
     )
@@ -41,67 +51,45 @@ def create_well_plot(df_draw_liq,
     m = 'markers'
     ml = 'markers+lines'
     colors = px.colors.qualitative.Pastel
-    clr_fact = 'rgba(99, 110, 250, 0.8)'
+    clr_fact = 'rgba(99, 110, 250, 0.7)'
     clr_pressure = '#C075A6'
 
     # Дебит жидкости
-    for ind, col in enumerate(df_draw_liq.columns):
-        clr = colors[ind]
-        mode = ml
-        if col == 'true':
-            mode = m
-            clr = clr_fact
+    trace = go.Scatter(name=f'LIQ: {_MODEL_NAMES["true"]}', x=df_draw_liq.index, y=df_draw_liq['true'],
+                       mode=m, marker=mark, line=dict(width=1, color=clr_fact))
+    fig.add_trace(trace, row=1, col=1)
 
-        trace = go.Scatter(name=f'{col}', x=df_draw_liq.index, y=df_draw_liq[col],
-                           mode=mode, marker=mark, line=dict(width=1, color=clr))
+    for ind, col in enumerate(df_draw_liq.columns):
+        if col == 'true':
+            continue
+        trace = go.Scatter(name=f'LIQ: {_MODEL_NAMES[col]}', x=df_draw_liq.index, y=df_draw_liq[col],
+                           mode=ml, marker=mark, line=dict(width=1, color=colors[ind]))
         fig.add_trace(trace, row=1, col=1)
 
     # Дебит нефти
-    for ind, col in enumerate(df_draw_oil.columns):
-        clr = colors[ind]
-        mode = ml
-        if col == 'true':
-            mode = m
-            clr = clr_fact
+    trace = go.Scatter(name=f'OIL: {_MODEL_NAMES["true"]}', x=df_draw_oil.index, y=df_draw_oil['true'],
+                       mode=m, marker=mark, line=dict(width=1, color=clr_fact))
+    fig.add_trace(trace, row=2, col=1)
 
-        trace = go.Scatter(name=f'{col}', x=df_draw_oil.index, y=df_draw_oil[col],
-                           mode=mode, marker=mark, line=dict(width=1, color=clr))
+    for ind, col in enumerate(df_draw_oil.columns):
+        if col == 'true':
+            continue
+        trace = go.Scatter(name=f'OIL: {_MODEL_NAMES[col]}', x=df_draw_oil.index, y=df_draw_oil[col],
+                           mode=ml, marker=mark, line=dict(width=1, color=colors[ind]))
         fig.add_trace(trace, row=2, col=1)
 
-    # Забойное давление
-    trace = go.Scatter(name=f'pressure', x=df_draw_oil.index, y=pressure[df_draw_oil.index],
-                       mode=m, marker=dict(size=4, color=clr_pressure))
-    fig.add_trace(trace, row=3, col=1)
+    for ind, col in enumerate(df_draw_oil.columns):
+        if col == 'true':
+            continue
+        deviations = compute_deviations(df_draw_oil['true'], df_draw_oil[col])
+        trace = go.Scatter(name=f'OIL ERR: {_MODEL_NAMES[col]}', x=deviations.index, y=deviations,
+                           mode=ml, marker=mark, line=dict(width=1, color=colors[ind]))
+        fig.add_trace(trace, row=3, col=1)
 
-    # 3, 2 test
-    fig.add_trace(
-        go.Scatter(
-            name=name_dev_liq + '_пьезо',
-            x=devs_liq_ftor.index,
-            y=devs_liq_ftor,
-            mode=ml, marker=mark,
-        ), row=3, col=2)
-    fig.add_trace(
-        go.Scatter(
-            name=name_dev_liq + '_ML',
-            x=devs_liq_wolfram.index,
-            y=devs_liq_wolfram,
-            mode=ml, marker=mark,
-        ), row=3, col=2)
-    fig.add_trace(
-        go.Scatter(
-            name=name_dev_oil + '_пьезо',
-            x=devs_oil_ftor.index,
-            y=devs_oil_ftor,
-            mode=ml, marker=mark,
-        ), row=3, col=2)
-    fig.add_trace(
-        go.Scatter(
-            name=name_dev_oil + '_ML',
-            x=devs_oil_wolfram.index,
-            y=devs_oil_wolfram,
-            mode=ml, marker=mark,
-        ), row=3, col=2)
+    # Забойное давление
+    trace = go.Scatter(name=f'Забойное давление', x=df_draw_oil.index, y=pressure[df_draw_oil.index],
+                       mode=m, marker=dict(size=4, color=clr_pressure))
+    fig.add_trace(trace, row=4, col=1)
 
     fig.add_vline(x=date_test, line_width=2, line_dash='dash')
 
