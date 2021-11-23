c2c288d5011ab9416d13b1250076cf9c561625b0
Konstantin Krechetov <Constantine.j.corp@gmail.com>
2021-11-23 16:40:26 +0300

    style: move class AppState to separate file

--- main_UI.py
+++ main_UI.py
@@ -7,43 +7,10 @@ import UI.pages.resume_app
 import UI.pages.specific_well
 import UI.pages.wells_map
 from config import Config as ConfigPreprocessor
+from UI.app_state import AppState
 from UI.cached_funcs import calculate_ftor, calculate_wolfram, calculate_ensemble, run_preprocessor
 from UI.config import FIELDS_SHOPS, DATE_MIN, DATE_MAX, DEFAULT_FTOR_BOUNDS
 from UI.data_processor import *
-
-
-class AppState(dict):
-    """
-    Example:
-    m = Map({'first_name': 'Eduardo'}, last_name='Pool', age=24, sports=['Soccer'])
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(AppState, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.items():
-                    self[k] = v
-        if kwargs:
-            for k, v in kwargs.items():
-                self[k] = v
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(AppState, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-    def __delattr__(self, item):
-        self.__delitem__(item)
-
-    def __delitem__(self, key):
-        super(AppState, self).__delitem__(key)
-        del self.__dict__[key]
 
 
 def initialize_session(session):
